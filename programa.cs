00f0e037cc46f36eba05b5425727cda7fc14176b
rodolfobraga18 <rodolfobraga46@gmail.com>
2025-09-16 20:03:17 -0300

    Add files via upload

---
+++ programa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using c_.models;
+
+namespace c_.obj
+{
+    class Programa
+{
+    static void Main(string[] args)
+    {
+        Estacionamento est = new Estacionamento();
+        est.executarmenu();
+    }
+}
+
+}
